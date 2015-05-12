b1f299ad2aa3d1441e1eb6fdc91be699e50b3c96
Ammaar Esmailjee <ammaar.esmailjee@gmail.com>
2015-05-11 21:39:07 -0400

    Version bump to v0.2
    Contains API breaking changes, all methods renamed

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import os
 from distutils.core import setup
 
 
 print "Welcome to Pyvona setup. Installing Pyvona..."
 
 setup(name='pyvona',
-      version='0.15',
+      version='0.2',
       description='Python IVONA Wrapper',
       author='Zachary Bears',
       author_email='bears.zachary@gmail.com',
       url='http://www.zacharybears.com/pyvona',
       py_modules=['pyvona'],
-)
+    )
 
 print "Please visit zacharybears.com/pyvona for tutorials, feature requests, and questions."
