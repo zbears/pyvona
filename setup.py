77aed5523917589d3006d807922d5db46e411506
Zachary Bears <bears.zachary@gmail.com>
2015-05-12 14:31:51 +0300

    Added dependencies to setup

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ print "Welcome to Pyvona setup. Installing Pyvona..."
 
 setup(name='pyvona',
       version='0.2',
-      description='Python IVONA Wrapper',
+      description='Python text-to-speech IVONA Wrapper',
       author='Zachary Bears',
       author_email='bears.zachary@gmail.com',
       url='http://www.zacharybears.com/pyvona',
       py_modules=['pyvona'],
+      install_requires=['requests']
     )
 
 print "Please visit zacharybears.com/pyvona for tutorials, feature requests, and questions."
